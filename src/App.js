57aa90131f25a83c917d48fbe6ffd4fa2b43d1cb
matthew-js-porter <matthew.js.porter@jpmchase.com>
2021-04-12 09:01:22 -0400

    updating title and logo

--- src/App.js
+++ src/App.js
@@ -5,18 +5,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1 className="title">sQReen Reader</h1>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button className="btn btn-primary btn-lg">Download</button>
       </header>
     </div>
   );
