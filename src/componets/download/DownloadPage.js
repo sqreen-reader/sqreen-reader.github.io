2e81f0159ad5b735ac8e057cb9e8411388b29b60
matthew-js-porter <matthew.js.porter@jpmchase.com>
2021-05-06 22:33:26 -0400

    release 2.0 of sQReen Reader
    Updated videos with new version of sQReen Reader
    removed Java installer and replaced with linix.

--- src/componets/download/DownloadPage.js
+++ src/componets/download/DownloadPage.js
@@ -2,32 +2,22 @@ import './DownloadPage.css';
 import {Tabs, Tab} from 'react-bootstrap';
 import {useState} from "react";
 import logo from '../../logo.svg';
-import downloadJava from '../../videos/java-run.mov';
+import downloadLinux from '../../videos/linux-install.mp4';
 import macInstall from '../../videos/mac-install.mov';
 import downloadUrls from './download-urls.json';
 import windowsInstall from '../../videos/windows-install.mp4';
 
 function DownloadPage() {
-    const [key, setkey] = useState('java');
+    const [key, setkey] = useState('mac');
     return (
         <div className='DownloadPage'>
             <header>
                 <img src={logo} className="Download-logo" alt="logo" />
                 <h1 className='Download-header'>Download sQReen Reader</h1>
                 <Tabs id='downloads' activeKey={key} onSelect={(key) => setkey(key)} className='Download-Tabs'>
-                    <Tab title='Java' eventKey='java'>
-                        <p>Requires Java 11 or higher</p>
-                        <a href={downloadUrls.java} className='btn btn-primary btn-lg'>Download</a>
-                        <video loop muted controls>
-                            <source src={downloadJava} type='video/mp4' />
-                        </video>
-                    </Tab>
+
                     <Tab title='macOS' eventKey='mac'>
                         <a href={downloadUrls.mac}  className='btn btn-primary btn-lg'>Download</a>
-                        <p>
-                            dmg is not signed. Will need to right-click on the application and hit open.
-                            Signed dmg coming soon.
-                        </p>
                         <video loop muted controls>
                             <source src={macInstall} type='video/mp4' />
                         </video>
@@ -36,6 +26,13 @@ function DownloadPage() {
                         <a href={downloadUrls.windows}  className='btn btn-primary btn-lg'>Download</a>
                         <video loop muted controls>
                             <source src={windowsInstall} type='video/mp4' />
+                        </video>
+                        <p>Note: exe is not signed.</p>
+                    </Tab>
+                    <Tab title='Linux' eventKey='java'>
+                        <a href={downloadUrls.linux} className='btn btn-primary btn-lg'>Download</a>
+                        <video loop muted controls>
+                            <source src={downloadLinux} type='video/mp4' />
                         </video>
                     </Tab>
                 </Tabs>
